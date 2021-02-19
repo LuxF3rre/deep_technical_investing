098919d7ca1a2a742dd8964a060a491ca34f4a73
Maurycy Blaszczak <mgblaszczak@gmail.com>
2021-02-19 14:09:18 +0100

    added dep, made universal

--- train_ai.py
+++ train_ai.py
@@ -18,4 +18,4 @@ dls = charts.dataloaders(chart_folder)
 learn = cnn_learner(dls, resnet18, metrics=error_rate)
 learn.fine_tune(4)
 
-learn.export(fname='apple_trading.pkl')
+learn.export(fname='deep_trading.pkl')
