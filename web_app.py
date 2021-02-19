087e1e60b4d24177aeeb23343244bd26974bd918
Maurycy Blaszczak <mgblaszczak@gmail.com>
2021-02-19 15:04:06 +0100

    added basic templating

--- web_app.py
+++ web_app.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from bottle import route, run, template
+# from bottle_sqlite import SQLitePlugin
+# import cherrypy  #for deployment
+
+app = Bottle()
+
+with app:
+    # install(SQLitePlugin(dbfile='database/db.db'))
+
+    @route('/')
+    def index():
+        return template('index')
+
+    run(host='localhost', port=8080, debug=True, reloader=True)
+    # bottle.run(server='cherrypy')  # for deployment
