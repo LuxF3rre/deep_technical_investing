df76e1b02b5979247299e98132ddee393e545b5d
Maurycy Blaszczak <mgblaszczak@gmail.com>
2021-02-19 13:50:25 +0100

    data gather and ai training

---
+++ get_data.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+import mplfinance as fplt
+import pandas as pd
+
+ticker_dataframe = pd.read_csv(
+    'data\\aapl.csv', index_col='Date', parse_dates=True).drop(['Volume'], axis=1)
+foresight = 7
+hindsight = 30
+number_of_data = 1000
+
+
+def get_ticker_slice(ticker_data, pointer, forecast_in_ticks, hindsight_in_ticks):
+    ticker_data_slice = ticker_data[:pointer + forecast_in_ticks]
+    ticker_data_slice = ticker_data_slice[-forecast_in_ticks -
+                                          hindsight_in_ticks:]
+    return ticker_data_slice
+
+
+def foresight_buy_or_sell(ticker_data, forecast_in_ticks):
+    return 'buy' if ticker_data['Close'][-forecast_in_ticks] < ticker_data['Close'][-1] else 'sell'
+
+
+def generate_sets(ticker_data, number_of_data, forecast_in_ticks, hindsight_in_ticks):
+    random_pointers = list()
+    for x in range(number_of_data):
+        random_pointers.append(random.randint(
+            hindsight_in_ticks, len(ticker_data) - forecast_in_ticks))
+
+    for x in range(number_of_data):
+        data = get_ticker_slice(
+            ticker_data, random_pointers[x], forecast_in_ticks, hindsight_in_ticks)
+        buy_or_sell = foresight_buy_or_sell(data, forecast_in_ticks)
+        save_to = 'data\\{}\\{}.png'.format(buy_or_sell, x)
+        fplt.plot(data[-hindsight_in_ticks:], type='candle',
+                  savefig=save_to)
+        print('Finished {} of {}'.format(x + 1, number_of_data))
+
+
+generate_sets(ticker_dataframe, number_of_data, foresight, hindsight)
---
+++ train_ai.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+ 
+from fastai.vision.widgets import *
+from fastbook import *
+from pathlib import Path
+
+recommendation_types = ['buy', 'sell']
+chart_folder = Path('../input/candlestick-charts-of-apple-stock')
+fns = get_image_files(chart_folder)
+
+charts = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label)
+
+dls = charts.dataloaders(chart_folder)
+
+learn = cnn_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(4)
+
+learn.export(fname='apple_trading.pkl')
